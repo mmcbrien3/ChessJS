535fddb9e51966f53e2df1b5e9c7b11989b0c46f
Matt McBrien <mmcbrien@amazon.com>
2021-03-27 23:47:06 -0400

    Update chess game to fill full window

--- src/main.js
+++ src/main.js
@@ -30,6 +30,10 @@ var config = {
         preload: preload,
         create: create,
         update: update,
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     }
 };
 
